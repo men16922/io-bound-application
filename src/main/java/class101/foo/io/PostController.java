82ae2eec612dc7423d2d6116dd3beb9ae8d1877f
men1692 <men16922@gmail.com>
2022-10-02 12:38:28 +0900

    feat : db에서 es를 사용하도록 변경

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -3,24 +3,14 @@ package class101.foo.io;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class PostController {
-
-    private static Integer PAGE_SIZE = 20;
-
     @Autowired
     PostRepository postRepository;
-
-    @Autowired
-    PostCacheService postCacheService;
-
     @Autowired
     ObjectMapper objectMapper;
 
@@ -35,27 +25,9 @@ public class PostController {
         return post;
     }
 
-    // 2. 글 목록을 페이징하여 반환
-    @GetMapping("/posts")
-    public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        if(page.equals(1)) {
-            return postCacheService.getFirstPostPage();
-        }
-        return postRepository.findAll(
-                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-        );
-    }
-
-
-    // 3. 글 번호로 조회
-    @GetMapping("/post/{id}")
-    public Post getPostById(@PathVariable("id") Long id) {
-        return postRepository.findById(id).get();
-    }
-
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
     public List<Post> findPostsByContent(@RequestParam String content) {
-        return postRepository.findByContentContains(content);
+        return postRepository.findByContent(content);
     }
 }
